1cfdf1f84645aeeaaa5a9aa018ed0ae4b3354c40
Brent <brent@Zipcoders-MacBook-Pro-3.local>
2023-06-30 17:27:27 -0400

    Empty classes CoreFeatures, ScientificFeatures created

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -5,6 +5,11 @@ package com.zipcodewilmington.scientificcalculator;
  */
 public class MainApplication {
     public static void main(String[] args) {
+        mainApplication.run();
+    }
+
+
+    public static void run(){
         Console.println("Welcome to my calculator!");
         String s = Console.getStringInput("Enter a string");
         Integer i = Console.getIntegerInput("Enter an integer");
@@ -13,5 +18,6 @@ public class MainApplication {
         Console.println("The user input %s as a string", s);
         Console.println("The user input %s as a integer", i);
         Console.println("The user input %s as a d", d);
+
     }
 }
