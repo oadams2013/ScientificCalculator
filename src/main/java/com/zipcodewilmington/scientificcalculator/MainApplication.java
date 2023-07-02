b454543d1810cc48a9fda8897bf890ff54e12269
Brent <BrentC0328@gmail.com>
2023-07-02 17:44:36 -0400

    cleaned wording

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -1,8 +1,5 @@
 package com.zipcodewilmington.scientificcalculator;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-
 /**
  * Created by leon on 2/9/18.
  */
@@ -13,7 +10,7 @@ public class MainApplication {
     }
 
 
-    public static void run(){
+    public static void run() {
         Double displayNumber = 0.0;
 
         boolean play = true;
@@ -21,85 +18,111 @@ public class MainApplication {
         Console.println(Console.introArt());
         Console.println(Console.calculatorWord());
 
-        Console.println("\n" + "Num: "+ displayNumber + "");
+        Console.println("\n" + "Num: " + displayNumber + "");
 
         displayNumber += Console.getIntegerInput("Enter number");
 
         String[] theCoreFeaturesArray = new String[]{"add", "multiply", "divide", "subtract", "exponent"};
 
 
+        while (play) {
 
-        while (play){
-
-            Console.println("\n" + "Num: "+ displayNumber + "");
+            Console.println("\n" + "Num: " + displayNumber + "");
 
             String operationChoice = Console.getStringInput("\n" + "Enter desired operation (ex: add)" + "\n" + "Or enter 'options' for list of operations.");
 
-            if (operationChoice.equalsIgnoreCase("bee")){
+            if (operationChoice.equalsIgnoreCase("bee")) {
                 Bee.main();
             }
 
-            if(operationChoice.equalsIgnoreCase("options")){
+            if (operationChoice.equalsIgnoreCase("options")) {
                 Console.println(Console.MathOptions());
-                operationChoice = Console.getStringInput("Enter desired math operation" );}
-
-
-            if(operationChoice.equalsIgnoreCase("clear")){
-                displayNumber = 0.0;Console.println(displayNumber + "");
-//                operationChoice = Console.getStringInput("Enter desired math operation" );
+                operationChoice = Console.getStringInput("Enter desired math operation");
             }
 
 
+            if (operationChoice.equalsIgnoreCase("clear")) {
+                displayNumber = 0.0;
+                Console.println(displayNumber + "");
 
+            }
 
-            //Can make an array with all operational words. If operationChoice is not a contained word in the array, Error message.
+            for (int i = 0; i < theCoreFeaturesArray.length; i++) {
 
-            for (int i = 0; i < theCoreFeaturesArray.length; i++){
-
-                if (theCoreFeaturesArray[i].equals(operationChoice)){
+                if (theCoreFeaturesArray[i].equals(operationChoice)) {
 
                     Integer enteredNumber = Console.getIntegerInput("Enter number");
 
-            if (operationChoice.equalsIgnoreCase("add")) {displayNumber = CoreFeatures.add(displayNumber, enteredNumber);}
+                    if (operationChoice.equalsIgnoreCase("add")) {
+                        displayNumber = CoreFeatures.add(displayNumber, enteredNumber);
+                    }
 
 
-            if (operationChoice.equalsIgnoreCase("multiply")) {
-              displayNumber = CoreFeatures.multiply(displayNumber, enteredNumber);
-            }
+                    if (operationChoice.equalsIgnoreCase("multiply")) {
+                        displayNumber = CoreFeatures.multiply(displayNumber, enteredNumber);
+                    }
 
-            if (operationChoice.equalsIgnoreCase("divide")) {
-                if (enteredNumber != 0) {
-                    displayNumber = CoreFeatures.division(displayNumber, enteredNumber);
-                } else {
-                    Console.println("Error: Cannot divide by 0" + "\n");
+                    if (operationChoice.equalsIgnoreCase("divide")) {
+                        if (enteredNumber != 0) {
+                            displayNumber = CoreFeatures.division(displayNumber, enteredNumber);
+                        } else {
+                            Console.println("Error: Cannot divide by 0" + "\n");
+                        }
+                    }
+
+                    if (operationChoice.equalsIgnoreCase("subtract")) {
+                        displayNumber = CoreFeatures.subtract(displayNumber, enteredNumber);
+                    }
+                    if (operationChoice.equalsIgnoreCase("exponent")) {
+                        displayNumber = CoreFeatures.exponent(displayNumber, enteredNumber);
+                    }
                 }
             }
-
-            if (operationChoice.equalsIgnoreCase("subtract")) {
-              displayNumber = CoreFeatures.subtract(displayNumber, enteredNumber);
+            if (operationChoice.equalsIgnoreCase("Sq")) {
+                displayNumber = CoreFeatures.square(displayNumber);
+            }
+            if (operationChoice.equalsIgnoreCase("sqrt")) {
+                displayNumber = CoreFeatures.squareRoot(displayNumber);
+            }
+            if (operationChoice.equalsIgnoreCase("inverse")) {
+                displayNumber = CoreFeatures.inverse(displayNumber);
+            }
+            if (operationChoice.equalsIgnoreCase("Factorial")) {
+                displayNumber = ScientificFeatures.getFactorial(displayNumber);
+            }
+            if (operationChoice.equalsIgnoreCase("Sin")) {
+                displayNumber = ScientificFeatures.getSin(displayNumber);
+            }
+            if (operationChoice.equalsIgnoreCase("Cos")) {
+                displayNumber = ScientificFeatures.getCos(displayNumber);
+            }
+            if (operationChoice.equalsIgnoreCase("Tan")) {
+                displayNumber = ScientificFeatures.getTan(displayNumber);
+            }
+            if (operationChoice.equalsIgnoreCase("ArcSin")) {
+                displayNumber = ScientificFeatures.getArcSin(displayNumber);
+            }
+            if (operationChoice.equalsIgnoreCase("ArcCos")) {
+                displayNumber = ScientificFeatures.getArcCos(displayNumber);
+            }
+            if (operationChoice.equalsIgnoreCase("ArcTan")) {
+                displayNumber = ScientificFeatures.getArcTan(displayNumber);
+            }
+            if (operationChoice.equalsIgnoreCase("invLog")) {
+                displayNumber = ScientificFeatures.invLog(displayNumber);
+            }
+            if (operationChoice.equalsIgnoreCase("radToDeg")) {
+                displayNumber = ScientificFeatures.radToDeg(displayNumber);
+            }
+            if (operationChoice.equalsIgnoreCase("log")) {
+                displayNumber = ScientificFeatures.log(displayNumber);
+            }
+            if (operationChoice.equalsIgnoreCase("invert")) {
+                displayNumber = CoreFeatures.invert(displayNumber);
             }
 
-            if (operationChoice.equalsIgnoreCase("exponent")){
-              displayNumber = CoreFeatures.exponent(displayNumber, enteredNumber);
-            }}}
 
-            if (operationChoice.equalsIgnoreCase("Sq")){displayNumber = CoreFeatures.square(displayNumber);}
-            if (operationChoice.equalsIgnoreCase("sqrt")){displayNumber = CoreFeatures.squareRoot(displayNumber);}
-            if (operationChoice.equalsIgnoreCase("inverse")){displayNumber = CoreFeatures.inverse(displayNumber);}
-            if (operationChoice.equalsIgnoreCase("Factorial")){displayNumber = ScientificFeatures.getFactorial(displayNumber);}
-            if (operationChoice.equalsIgnoreCase("Sin")){displayNumber = ScientificFeatures.getSin(displayNumber);}
-            if (operationChoice.equalsIgnoreCase("Cos")){displayNumber = ScientificFeatures.getCos(displayNumber);}
-            if (operationChoice.equalsIgnoreCase("Tan")){displayNumber = ScientificFeatures.getTan(displayNumber);}
-            if (operationChoice.equalsIgnoreCase("ArcSin")){displayNumber = ScientificFeatures.getArcSin(displayNumber);}
-            if (operationChoice.equalsIgnoreCase("ArcCos")){displayNumber = ScientificFeatures.getArcCos(displayNumber);}
-            if (operationChoice.equalsIgnoreCase("ArcTan")){displayNumber = ScientificFeatures.getArcTan(displayNumber);}
-            if (operationChoice.equalsIgnoreCase("invLog")){displayNumber = ScientificFeatures.invLog(displayNumber);}
-            if (operationChoice.equalsIgnoreCase("radToDeg")){displayNumber = ScientificFeatures.radToDeg(displayNumber);}
-            if (operationChoice.equalsIgnoreCase("log")){displayNumber = ScientificFeatures.log(displayNumber);}
-            if (operationChoice.equalsIgnoreCase("invert")){displayNumber = CoreFeatures.invert(displayNumber);}
-
-
-            Console.println(displayNumber + "");
+            Console.println("Num: " + displayNumber + "");
 
 
             String exitAnswer = Console.getStringInput("Continue calculating? Enter Y/N");
@@ -108,28 +131,6 @@ public class MainApplication {
                 play = false;
             }
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//        String s = Console.getStringInput("Enter a string");
-//        Integer i = Console.getIntegerInput("Enter an integer");
-//        Double d = Console.getDoubleInput("Enter a double.");
-//
-//        Console.println("The user input %s as a string", s);
-//        Console.println("The user input %s as a integer", i);
-//        Console.println("The user input %s as a d", d);
 
 
     }
