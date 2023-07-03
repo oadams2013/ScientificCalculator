f9822a8167c772c514a831b28f85599d1120230e
Liz Kinker <Liz.Kinker@gmail.com>
2023-07-03 13:05:27 -0400

    fixed spacing, removed if statements

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -24,7 +24,7 @@ public class MainApplication {
 
         displayNumber += Console.getIntegerInput("Enter number");
 
-        String[] theCoreFeaturesArray = new String[]{"add", "+", "multiply","mult","*", "divide", "div", "/", "subtract","sub", "-", "exponent", "exp"};
+        String[] theCoreFeaturesArray = new String[]{"add", "+", "multiply", "mult", "*", "divide", "div", "/", "subtract", "sub", "-", "exponent", "exp"};
 
 
         while (play) {
@@ -34,10 +34,12 @@ public class MainApplication {
 
             String operationChoice = Console.getStringInput("\n" + "Enter desired operation (ex: add)" + "\n" + "[C] Clear   [O] Option   [Q] Quit");
 
-            if(operationChoice.equalsIgnoreCase("q")) {
+            if (operationChoice.equalsIgnoreCase("q")) {
                 play = false;
             }
-            if (operationChoice.equalsIgnoreCase("bee")) { Bee.main();}
+            if (operationChoice.equalsIgnoreCase("bee")) {
+                Bee.main();
+            }
 
             if (operationChoice.equalsIgnoreCase("o")) { //changed "options" to "o"
                 Console.println(Console.MathOptions());
@@ -58,57 +60,40 @@ public class MainApplication {
 
                     Integer enteredNumber = Console.getIntegerInput("Enter number");
                     switch (operationChoice.toLowerCase()) {
-                        case "add" : case "+":
+                        case "add":
+                        case "+":
                             displayNumber = CoreFeatures.add(displayNumber, enteredNumber);
                             break;
-                        case "multiply" : case "mult" : case "*":
+                        case "multiply":
+                        case "mult":
+                        case "*":
                             displayNumber = CoreFeatures.multiply(displayNumber, enteredNumber);
                             break;
-                        case "divide" : case "div": case "/":
+                        case "divide":
+                        case "div":
+                        case "/":
                             if (enteredNumber != 0) {
                                 displayNumber = CoreFeatures.division(displayNumber, enteredNumber);
                             } else {
                                 Console.println("Error: Cannot divide by 0" + "\n");
                             }
                             break;
-                        case "subtract": case "sub": case "-":
+                        case "subtract":
+                        case "sub":
+                        case "-":
                             displayNumber = CoreFeatures.subtract(displayNumber, enteredNumber);
                             break;
-                        case "exponent": case "exp":
+                        case "exponent":
+                        case "exp":
                             displayNumber = CoreFeatures.exponent(displayNumber, enteredNumber);
                             break;
                     }
 
-                    /* if (operationChoice.equalsIgnoreCase("add")) {
-                        displayNumber = CoreFeatures.add(displayNumber, enteredNumber);
-                    }
 
-
-
-                    if (operationChoice.equalsIgnoreCase("multiply")) {
-                        displayNumber = CoreFeatures.multiply(displayNumber, enteredNumber);
-                    }
-
-                    if (operationChoice.equalsIgnoreCase("divide")) {
-                        if (enteredNumber != 0) {
-                            displayNumber = CoreFeatures.division(displayNumber, enteredNumber);
-                        } else {
-                            Console.println("Error: Cannot divide by 0" + "\n");
-                        }
-                    }
-
-                    if (operationChoice.equalsIgnoreCase("subtract")) {
-                        displayNumber = CoreFeatures.subtract(displayNumber, enteredNumber);
-                    }
-                    if (operationChoice.equalsIgnoreCase("exponent")) {
-                        displayNumber = CoreFeatures.exponent(displayNumber, enteredNumber);
-                    }
-
-                     */
                 }
             }
 
-            //if (operationChoice.equalsIgnoreCase("+")) { operationChoice = "add";}
+
             if (operationChoice.equalsIgnoreCase("Sq")) {
                 displayNumber = CoreFeatures.square(displayNumber);
             }
@@ -116,33 +101,44 @@ public class MainApplication {
                 displayNumber = CoreFeatures.squareRoot(displayNumber);
             }
             if (operationChoice.equalsIgnoreCase("inverse")) {
-                if (displayNumber != 0){
+                if (displayNumber != 0) {
                     displayNumber = CoreFeatures.inverse(displayNumber);
-                } else { Console.println("\n"+ "Error: Inverse by 0 undefined" + "\n");
+                } else {
+                    Console.println("\n" + "Error: Inverse by 0 undefined" + "\n");
+                }
+            }
 
-            } }
-            // Try Switch Case
+            // Try Switch Case because it allows for cleaner input options
             switch (operationChoice.toLowerCase()) {
 
                 case "factorial":
                     displayNumber = ScientificFeatures.getFactorial(displayNumber);
                     break;
-                case "sin": case "sine":
+                case "sin":
+                case "sine":
                     displayNumber = ScientificFeatures.getSin(displayNumber);
                     break;
-                case "cos": case "cosine":
+                case "cos":
+                case "cosine":
                     displayNumber = ScientificFeatures.getCos(displayNumber);
                     break;
-                case "tan": case "tangent":
+                case "tan":
+                case "tangent":
                     displayNumber = ScientificFeatures.getTan(displayNumber);
                     break;
-                case "arcsin": case "invsin": case "inverse sin":
+                case "arcsin":
+                case "invsin":
+                case "inverse sin":
                     displayNumber = ScientificFeatures.getArcSin(displayNumber);
                     break;
-                case "arccos": case "invcos": case "inverse cos":
+                case "arccos":
+                case "invcos":
+                case "inverse cos":
                     displayNumber = ScientificFeatures.getArcCos(displayNumber);
                     break;
-                case "arctan": case "invtan": case "inverse tan":
+                case "arctan":
+                case "invtan":
+                case "inverse tan":
                     displayNumber = ScientificFeatures.getArcTan(displayNumber);
                     break;
                 case "log":
@@ -163,46 +159,7 @@ public class MainApplication {
             }
 
 
-             /*  // Remove If statements
-
-            if (operationChoice.equalsIgnoreCase("Factorial")) {
-                displayNumber = ScientificFeatures.getFactorial(displayNumber);
-            }
-            if (operationChoice.equalsIgnoreCase("Sin")) {
-                displayNumber = ScientificFeatures.getSin(displayNumber);
-            }
-
-            if (operationChoice.equalsIgnoreCase("Cos")) {
-                displayNumber = ScientificFeatures.getCos(displayNumber);
-            }
-            if (operationChoice.equalsIgnoreCase("Tan")) {
-                displayNumber = ScientificFeatures.getTan(displayNumber);
-            }
-            if (operationChoice.equalsIgnoreCase("ArcSin")) {
-                displayNumber = ScientificFeatures.getArcSin(displayNumber);
-            }
-            if (operationChoice.equalsIgnoreCase("ArcCos")) {
-                displayNumber = ScientificFeatures.getArcCos(displayNumber);
-            }
-            if (operationChoice.equalsIgnoreCase("ArcTan")) {
-                displayNumber = ScientificFeatures.getArcTan(displayNumber);
-            }
-            if (operationChoice.equalsIgnoreCase("invLog")) {
-                displayNumber = ScientificFeatures.invLog(displayNumber);
-            }
-            if (operationChoice.equalsIgnoreCase("radToDeg")) {
-                displayNumber = ScientificFeatures.radToDeg(displayNumber);
-            }
-            if (operationChoice.equalsIgnoreCase("log")) {
-                displayNumber = ScientificFeatures.log(displayNumber);
-            }
-            if (operationChoice.equalsIgnoreCase("invert")) {
-                displayNumber = CoreFeatures.invert(displayNumber);
-            }
-            
-              */
-
-            //calcDisplay();
+            // Edited out.  Redundant
             //Console.println("Num: " + displayNumber + "");
 
             /*
@@ -220,7 +177,7 @@ public class MainApplication {
 
     public static void calcDisplay() {
         System.out.println(" ---------------------");
-        System.out.println("|   "+String.format("%15.2f", displayNumber)+"   |");
+        System.out.println("|   " + String.format("%15.2f", displayNumber) + "   |");
         System.out.println(" ---------------------");
     }
 }
